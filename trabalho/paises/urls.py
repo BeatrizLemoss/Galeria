6dcec0afb8fdb7e3b214dbc7f092b639fa584545
júlia <162610003+juliamacedo63@users.noreply.github.com>
2025-04-08 10:07:53 -0300

    Add files via upload

--- trabalho/paises/urls.py
+++ trabalho/paises/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Certifique-se de que a URL está corretamente configurada
+    path('', views.index, name='index'),
+    path('sobre/',views.sobre, name = 'sobre')  # Certifique-se de que a URL está corretamente configurada
 ]
